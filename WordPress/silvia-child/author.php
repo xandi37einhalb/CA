017e351b0b8a0d4f611e4933363893ea074eeb08
Lukas Kamber <lukas.kamber@klink.ch>
2016-02-06 22:38:11 +0100

    Autoren: Nur Vorname und anderer Link zu Profilseite

--- WordPress/silvia-child/author.php
+++ WordPress/silvia-child/author.php
@@ -8,7 +8,7 @@
 				<header class="page-header" <?php hybrid_attr( 'entry-author' ) ?>>
 					<?php echo get_avatar( is_email( get_the_author_meta( 'user_email' ) ), apply_filters( 'silvia_author_bio_avatar_size', 90 ), '', esc_attr( get_the_author() ) ); ?>
 					<h1 class="page-title">
-						<span class="vcard" itemprop="name"><?php echo esc_attr( get_the_author() ); ?></span>
+						<span class="vcard" itemprop="name"><?php echo stripslashes( get_the_author_meta( 'first_name' ) ); ?></span>
 					</h1>
 					<div class="description">
 						<p class="bio" itemprop="description"><?php echo stripslashes( get_the_author_meta( 'description' ) ); ?></p>
@@ -18,7 +18,7 @@
 							$url = get_the_author_meta('url');
 							$urlparsed = parse_url(get_the_author_meta('url'));
 							if (preg_match('/cycling-adventures/', $urlparsed['host'])) {
-								print '<a href="' . $url . '" target="_blank">' . get_the_author_meta('first_name') . ' bei Cycling Adventures Rennrad Reisen</a>';
+								print '<a href="' . $url . '" target="_blank">Mehr über ' . get_the_author_meta('first_name') . ' bei Cycling Adventures Rennrad Reisen</a>';
 							}
 							else {
 								print '<a href="' . $url . '" target="_blank">' . $urlparsed['host'] . '</a>';							
