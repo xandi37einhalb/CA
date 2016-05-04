bdd1d25211d728eb725b17a2f69d4d1680673520
Lutz Goldbecker <lutz@stessaonda.net>
2016-05-04 10:42:59 +0200

    Notwendige Änderung für WP4.5.1

--- IT/Roxima-Theme/scripts.js
+++ IT/Roxima-Theme/scripts.js
@@ -75,7 +75,7 @@ jQuery(function( $ ) {
 	----------------------------------------- */
 	var offset = 80;
 
-	$( 'a[href*=#]:not([href=#])' ).not( '.mobile-trigger, .mm-btn' ).click(function() {
+	$( 'a[href*="#"]:not([href="#"])' ).not( '.mobile-trigger, .mm-btn, .mm-next' ).click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
